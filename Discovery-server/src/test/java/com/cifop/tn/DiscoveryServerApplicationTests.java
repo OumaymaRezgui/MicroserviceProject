b160d33df84381c71e1f65e58a05246af9bdcd28
oumay1995 <rezguioumayma95@gmail.com>
2024-11-08 22:47:15 +0100

    third commit

---
+++ Discovery-server/src/test/java/com/cifop/tn/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cifop.tn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
